297d69e18b3337403d5c37375ba067c385cecb80
OmarZen <omarwaleedzenhom2002@gmail.com>
2024-12-04 16:39:23 +0200

    firebase setup for all devices and platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:asroo_store/asroo_store_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AsrooStoreApp());
 }
 
