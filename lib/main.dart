6b09df599a645147220c9e974e0eee130a679729
OmarZen <omarwaleedzenhom2002@gmail.com>
2024-12-04 18:01:30 +0200

    create env files done and make it in a gloable variables with singlton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/app/env.variables.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -6,6 +7,9 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
